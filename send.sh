6b97972545a88c33404e236f2831a59ef34fbad4
techtronix-868 <kahaan2912@gmail.com>
2023-01-12 14:17:30 -1000

    {changed}

--- send.sh
+++ send.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# add to repo
+
+git add . 
+
+# git commit repository
+
+git commit -m "{$1}"
+
+# pushing to remote repo
+
+git push
 # git pull repository
 git pull
 
 # Store the password in a variable
-password="=yXEnU%a"
 
 # copy file to server using sshpass
 # chnage the source path /mnt/c/Users/kahaa/Documents/Github/RDNT/website/* according to your soruce file path
-sshpass -p =yXEnU%a scp -r /mnt/c/Users/kahaa/Documents/Github/RDNT/website/* 477grp18@shay.ecn.purdue.edu:~/web/
+sshpass -f ../../pass.txt scp -r /mnt/c/Users/kahaa/Documents/Github/RDNT/website/* 477grp18@shay.ecn.purdue.edu:~/web/
