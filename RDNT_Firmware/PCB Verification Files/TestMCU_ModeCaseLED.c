ff39c253ac81c39639a18a08e4eaf332e8583a7a
anurag423 <55445184+anurag423@users.noreply.github.com>
2023-04-06 19:19:09 -0400

    added caseled, still not done

--- RDNT_Firmware/PCB Verification Files/TestMCU_ModeCaseLED.c
+++ RDNT_Firmware/PCB Verification Files/TestMCU_ModeCaseLED.c
@@ -12,8 +12,7 @@
 // This program confirms the mode switch button is operating by changing the color of the case LED
 // Might be slightly buggy, didnt get to test it completely
 
-#define GPIO_MODE_INPUT 27
-#define GPIO_INPUT_PIN_SEL ((1ULL << GPIO_MODE_INPUT))
+#define GPIO_INPUT_27 GPIO_NUM_27
 #define ESP_INTR_FLAG_DEFAULT 0
 
 #define RMT_LED_STRIP_RESOLUTION_HZ 10000000 // 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
@@ -22,10 +21,11 @@
 #define LED_NUMBERS 4
 #define CHASE_SPEED_MS 100
 
-volatile int state = 1; // 0 = Microphone, 1 = Aux, 2 = Bluetooth
+volatile int state = 1; // 001 = Microphone, 010 = Aux, 100 = Bluetooth
 volatile int green = 255;
 volatile int red = 0;
 volatile int blue = 0;
+volatile bool transition = false;
 
 static const char *TAG = "example";
 
@@ -34,42 +34,31 @@ static uint8_t led_strip_pixels[LED_NUMBERS * 3];
 rmt_channel_handle_t led_chan = NULL;
 rmt_encoder_handle_t led_encoder = NULL;
 
-static QueueHandle_t gpio_evt_queue = NULL;
-
 static void IRAM_ATTR gpio_isr_handler(void *arg)
 {
-    uint32_t gpio_num = (uint32_t)arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+    transition = true;
 }
 
 static void mode_switch(void *arg)
 {
-    uint32_t io_num;
     for (;;)
     {
-        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY))
+        if (transition)
         {
-            printf("GPIO[%" PRIu32 "] intr, val: %d\n", io_num, gpio_get_level(io_num));
             state = (state * 2) % 7;
+            printf("%i\n", state);
+            transition = false;
             green = (state & 0b001) * 255;
-            red = ((state & 0b010) * 255) >> 1;
-            blue = ((state & 0b100) * 255) >> 2;
+            red = ((state & 0b010) >> 1) * 255;
+            blue = ((state & 0b100) >> 2) * 255;
+            for (int j = 0; j < LED_NUMBERS * 3; j++)
+            {
+                led_strip_pixels[j * 3 + 0] = green; // green
+                led_strip_pixels[j * 3 + 1] = red;   // red
+                led_strip_pixels[j * 3 + 2] = blue;  // blue
+            }
         }
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
-
-static void CaseLED(void *arg)
-{
-    for (;;)
-    {
-        for (int j = 0; j < LED_NUMBERS * 3; j++)
-        {
-            led_strip_pixels[j * 3 + 0] = green; // green
-            led_strip_pixels[j * 3 + 1] = red;   // red
-            led_strip_pixels[j * 3 + 2] = blue;  // blue
-        }
-        vTaskDelay(pdMS_TO_TICKS(CHASE_SPEED_MS));
     }
 }
 
@@ -101,36 +90,42 @@ void app_main(void)
         .loop_count = 0, // no transfer loop
     };
 
+    for (int j = 0; j < LED_NUMBERS * 3; j++)
+    {
+        led_strip_pixels[j * 3 + 0] = green; // green
+        led_strip_pixels[j * 3 + 1] = red;   // red
+        led_strip_pixels[j * 3 + 2] = blue;  // blue
+    }
+
     // zero-initialize the config structure.
     gpio_config_t io_conf = {};
 
-    // interrupt of rising edge
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
-    // bit mask of the pins, use GPIO4/5 here
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    // bit mask of the pins, use GPIO27 here
+    io_conf.pin_bit_mask = (1ULL << GPIO_INPUT_27);
     // set as input mode
     io_conf.mode = GPIO_MODE_INPUT;
-    // enable pull-up mode
+    // disable pull-down mode
+    io_conf.pull_down_en = 0;
+    // disable pull-up mode
     io_conf.pull_up_en = 1;
-    gpio_config(&io_conf);
+    // interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
 
-    // create a queue to handle gpio event from isr
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t)); // TODO: experiment with queue size
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+
     // start gpio task
     xTaskCreate(mode_switch, "mode_switch", 2048, NULL, 10, NULL);
 
     // install gpio isr service
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     // hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_MODE_INPUT, gpio_isr_handler, (void *)GPIO_MODE_INPUT);
+    gpio_isr_handler_add(GPIO_INPUT_27, gpio_isr_handler, (void *)GPIO_INPUT_27);
 
     printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    xTaskCreate(CaseLED, "CaseLED", 2048, NULL, 10, NULL);
 
     while (1)
     {
         ESP_ERROR_CHECK(rmt_transmit(led_chan, led_encoder, led_strip_pixels, sizeof(led_strip_pixels), &tx_config));
-        vTaskDelay(pdMS_TO_TICKS(CHASE_SPEED_MS));
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
